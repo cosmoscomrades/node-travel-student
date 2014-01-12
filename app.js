6fb84b6f4591f19bd924f9640ea8a1d2eb94c08a
MATTHEW LARSON <matthewlarson@MATTHEWs-MacBook-Air-2.local>
2014-01-12 18:27:33 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ appHelper.runApp(function(app, db) {
 	console.log("My app is running!");
 
 	app.get("/hello", function (req, resp) {
-		resp.write("HELLO WORLD!");
+		resp.write("HELLO WORLD! My name is Matt");
 		resp.end();
 	});
+
+
+	app.get("/html", function (req, resp) {
+		resp.write("<html><body><h1>Woo!! Node!!</h1><button></body></html>");
+		resp.end();
+	});
+
+	app.get("/"), function (req, resp) {
+		db.findArray({}, function(arr) {
+			resp.render("index", { locations: arr });	
+		});
+
+	});
+
 })
+
